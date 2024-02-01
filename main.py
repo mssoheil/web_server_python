ba664dfc02853b8eb332301ae82af26ed1198e2b
mssoehil <mssoheil88@yahoo.com>
2024-02-01 06:10:02 -0500

    feat: open redirection

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+import requests
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://theUserName:thePassword@localhost/mydb"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://myuser:kali@localhost/mydb"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
@@ -71,6 +72,41 @@ def register():
 
     return render_template("register.html", message=message)
 
+@app.route("/redirect", methods=["GET"])
+def redirecting():
+    return render_template("redirect.html")
+
+@app.route("/open", methods=["GET"])
+def open():
+    # getting url value from the url of the page
+    targetUrl = request.args.get("url")
+    # if action exists use that else defaults to "redirect"
+    action = request.args.get("action", "redirect")
+
+    # open redirection
+    if action == "redirect":
+        return redirect(targetUrl)
+    
+    # SSRF
+    elif action == "fetch":
+        try:
+            response = requests.get(targetUrl, timeout = 5)
+
+            if response.status_code == 200:
+                return f"received ok, {targetUrl}"
+            else:
+                return f"received {response.status_code} from {targetUrl}"
+            
+        except requests.ConnectionError:
+            return f"conncection refused by {targetUrl}"
+        
+        except requests.Timeout:
+            return f"connection timed out by {targetUrl}"
+        
+        except Exception as error:
+            return str(error)
+
+    return "specify action and url"
 
 if __name__ == "__main__":
     # only for development
